443bb4c1a7834f1e1d58366e6ae3fcb676ed9a00
Bijan k <80640045+Bijan-K@users.noreply.github.com>
2023-07-20 09:40:59 +0330

    setting up the basics

---
+++ components/Layout.js
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+
+export default function Layout({ title, keywords, description, children }) {
+  return (
+    <div className="h-screen">
+      <Head>
+        <title>{title}</title>
+        <meta name="keywords" content={keywords} />
+        <meta name="keywords" content={description} />
+        <link href="/favicon.ico" rel="icon" />
+      </Head>
+
+      <main className="w-full h-full">{children}</main>
+    </div>
+  );
+}
+
+Layout.defaultProps = {
+  title: 'Title',
+  keywords: 'markdown,preview,coding',
+  description: 'markdown preview client',
+};
