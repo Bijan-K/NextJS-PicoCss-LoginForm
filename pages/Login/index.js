9b75792d1b944ff956902438be95814f3576c640
Bijan k <80640045+Bijan-K@users.noreply.github.com>
2023-07-25 21:32:04 +0330

    progress

--- pages/Login/index.js
+++ pages/Login/index.js
@@ -16,7 +16,10 @@ export default function Login() {
             type="password"
             placeholder="Password"
           />
-          <Link href="/">forgot password?</Link>
+
+          <Link href="/Login/#test-popup" data-effect="mfp-move-horizontal">
+            forgot password?
+          </Link>
           <Link href="/myaccount">
             <button className="margin" type="submit">
               Login
@@ -26,10 +29,19 @@ export default function Login() {
       </article>
       <article className="container">
         <div className="middle">
-          <h3>example</h3>
-          <h6> username:name1 password:12345678Ab</h6>
+          <h3>Use this credentials to login</h3>
+          <h6>
+            {' '}
+            username: name1 <br /> password: 12345678Ab
+          </h6>
         </div>
       </article>
+      <div id="test-popup" class="white-popup mfp-with-anim mfp-hide">
+        You may put any HTML here. This is dummy copy. It is not meant to be
+        read. It has been placed here solely to demonstrate the look and feel of
+        finished, typeset text. Only for show. He who searches for meaning here
+        will be sorely disappointed.
+      </div>
     </>
   );
 }
